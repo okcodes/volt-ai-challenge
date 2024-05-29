2f7b08e8b376b148ffbdd3818dfda179636faf50
okcodes <80954089+okcodes@users.noreply.github.com>
2024-05-28 21:28:15 -0600

    Prepare markup for fps example

--- src/FloorPlan.tsx
+++ src/FloorPlan.tsx
@@ -1,3 +1,16 @@
+import './FloorPlan.css'
+
 export const FloorPlan = () => {
-  return <div>floor plan</div>
+  return <div id="blocker">
+    <div id="instructions">
+      <p style={{fontSize: '36px'}}>
+        Click to play
+      </p>
+      <p>
+        Move: WASD<br/>
+        Jump: SPACE<br/>
+        Look: MOUSE
+      </p>
+    </div>
+  </div>
 }
