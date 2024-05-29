325402db4b6d590d3bd2b33998404d441430dc5c
okcodes <80954089+okcodes@users.noreply.github.com>
2024-05-28 22:24:03 -0600

    Refactor fn to spawn cubes

--- src/FloorPlan.tsx
+++ src/FloorPlan.tsx
@@ -11,6 +11,23 @@ export const FloorPlan = () => {
   const scene = useRef(new THREE.Scene());
   const boxGeometry = useRef(new THREE.BoxGeometry(20, 20, 20).toNonIndexed());
 
+  const spawnRandomCube = () => {
+    const boxMaterial = new THREE.MeshPhongMaterial({
+      specular: 0xffffff,
+      flatShading: true,
+      vertexColors: true
+    });
+    boxMaterial.color.setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75, THREE.SRGBColorSpace);
+
+    const box = new THREE.Mesh(boxGeometry.current, boxMaterial);
+    box.position.x = Math.floor(Math.random() * 20 - 10) * 20;
+    box.position.y = Math.floor(Math.random() * 20) * 20 + 10;
+    box.position.z = Math.floor(Math.random() * 20 - 10) * 20;
+
+    scene.current.add(box);
+    objects.current.push(box);
+  }
+
   const onKeyPressed = (event: KeyboardEvent) => {
     if (event.code === 'KeyM') {
       console.log('M pressed')
@@ -202,22 +219,7 @@ export const FloorPlan = () => {
       boxGeometry.current.setAttribute('color', new THREE.Float32BufferAttribute(colorsBox, 3));
 
       for (let i = 0; i < 500; i++) {
-
-        const boxMaterial = new THREE.MeshPhongMaterial({
-          specular: 0xffffff,
-          flatShading: true,
-          vertexColors: true
-        });
-        boxMaterial.color.setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75, THREE.SRGBColorSpace);
-
-        const box = new THREE.Mesh(boxGeometry.current, boxMaterial);
-        box.position.x = Math.floor(Math.random() * 20 - 10) * 20;
-        box.position.y = Math.floor(Math.random() * 20) * 20 + 10;
-        box.position.z = Math.floor(Math.random() * 20 - 10) * 20;
-
-        scene.current.add(box);
-        objects.current.push(box);
-
+        spawnRandomCube()
       }
 
       //
